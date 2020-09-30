c7842936d7f5b9fe7cf212faab7673c0dd0ede3d
Guillou33 <autiergilles@gmail.com>
2020-09-30 16:39:28 +0200

    cowsay

---
+++ index.js
@@ -0,0 +1,11 @@
+const eleveChoisi = require('./information.js');
+eleveChoisi();
+
+const cow = require('cowsay');
+console.log(
+  cow.say({
+    text: 'Hello boy!',
+    e: 'Xx',
+    T: 'U',
+  })
+);
---
+++ information.js
@@ -0,0 +1,10 @@
+let eleve = {
+  name: 'Gilles',
+  campus: 'Lyon',
+};
+
+const monEleve = () => {
+  console.log(`Hi I'm ${eleve.name} and I'm in the campus ${eleve.campus}`);
+};
+
+module.exports = monEleve;
